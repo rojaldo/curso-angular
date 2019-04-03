8f9726531ee3c9c314b062700db79165ab1b6e29
Daniel Rojo Pérez <rojaldo@yahoo.com>
2019-04-03 13:59:54 +0200

    pipe alcohol

--- src/app/pipes/alcohol.pipe.ts
+++ src/app/pipes/alcohol.pipe.ts
@@ -6,6 +6,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class AlcoholPipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
+    if (typeof value === 'number') {
+      return Math.floor(value) + ',' + Math.round((value % 1) * 10) + 'º';
+    } else {
+      console.error('Wrong parameter type: ' + (typeof value));
+    }
     return value;
   }
 
