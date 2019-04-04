8c16c7d1412c1d0f1ecbee4077be532c7807dfc9
Daniel Rojo Pérez <rojaldo@yahoo.com>
2019-04-04 09:51:15 +0200

    pipe alcohol with parameters

--- src/app/pipes/alcohol.pipe.ts
+++ src/app/pipes/alcohol.pipe.ts
@@ -5,13 +5,42 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class AlcoholPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
+  transform(value: any, ...args: any): any {
+    let symbol = ',';
+    let graduation = 'º';
+    console.log(args);
     if (typeof value === 'number') {
-      return Math.floor(value) + ',' + Math.round((value % 1) * 10) + 'º';
+      if (args !== undefined) {
+        switch (args[0]) {
+          case ',':
+            symbol = ',';
+            break;
+          case '.':
+            symbol = '.';
+            break;
+          default:
+            symbol = ',';
+        }
+        console.log(args[1]);
+        switch (args[1]) {
+          case 'º':
+            graduation = 'º';
+            break;
+          case '%':
+            graduation = '%';
+            break;
+          default:
+            graduation = '%';
+        }
+        return Math.floor(value) + symbol + Math.round((value % 1) * 10) + graduation;
+      } else {
+        return Math.floor(value) + symbol + Math.round((value % 1) * 10) + 'º';
+      }
     } else {
       console.error('Wrong parameter type: ' + (typeof value));
+      return value;
     }
-    return value;
   }
-
 }
+
+
